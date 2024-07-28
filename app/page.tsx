6fe431ca7eec0e88466872a25840dfa92e722591
Md Istiak Ahmed <istinishat@gmail.com>
2024-07-28 13:51:41 +0600

    Add card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import CardComponent from "@/components/card/CardComponent";
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="h-[100vh]">hello</div>;
+  return (
+    <div className="h-[100vh]">
+      <CardComponent />
+    </div>
+  );
 }
